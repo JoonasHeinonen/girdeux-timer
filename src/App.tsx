cd9dc07b57275b755b83b646d436bf9df3b3a246
Joonas Heinonen <mrdanglars@gmail.com>
2024-04-26 21:02:51 +0300

    girdeux-counter: First commit.

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>Counter App</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
